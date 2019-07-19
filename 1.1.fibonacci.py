8d774c7bdb824a36d0f5b969fd749c6ebbf43239
Ora Martindale <ora@martindale.org>
2019-07-18 21:35:40 -0500

    fib2(): Handle when function is called with 0 or 1 to prevent infinite recursion

--- 1.1.fibonacci.py
+++ 1.1.fibonacci.py
@@ -3,5 +3,11 @@ def fib1(n):
     """ Infinite Recursion Example """
     return fib1(n - 1) + fib1(n - 2)
 
+def fib2(n):
+    if n < 2:
+        return n
+    return fib2(n - 1) + fib2(n - 2)
+
 if __name__ == '__main__':
-    print(fib1(5))
+    print(fib2(5))
+    print(fib2(10))
