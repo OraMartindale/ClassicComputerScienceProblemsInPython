3235cc1f54837e9f782a700820a1fe1e04596fef
Ora Martindale <ora@martindale.org>
2019-07-18 21:32:26 -0500

    fixup! First Fibonacci example

--- 1.1.fibonacci.py
+++ 1.1.fibonacci.py
@@ -1,7 +1,7 @@
 
-def fib1(n: int) -> int:
+def fib1(n):
     """ Infinite Recursion Example """
-    return fib(n - 1) + fib(n - 2)
+    return fib1(n - 1) + fib1(n - 2)
 
 if __name__ == '__main__':
     print(fib1(5))
